519ed5d7fe7f422dbd3b1f1dc01b376cc944671c
tierney03 <tierney_03@mac.com>
2023-01-11 09:13:10 -0600

    Initial Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 1, 11);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 170.22;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
